961734665843bbc55e1aa2925f48e88b3bed94d6
Noman <mdnomankaif55@gmail.com>
2025-08-11 18:47:49 +0530

    from numan

---
+++ src/pages/Contact/index.jsx
@@ -0,0 +1,16 @@
+import HeroSection from '@/components/common/HeroSection';
+import React from 'react';
+
+export const Contactus = (props) => {
+  return (
+    <div>
+      <section>
+        <HeroSection
+          subtitle="contact us"
+          title="Let’s connect"
+          description="Let’s discuss how our experts can solve your IT challenges and drive your business forward."
+        />
+      </section>
+    </div>
+  );
+};
